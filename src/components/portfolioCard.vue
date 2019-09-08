25806b4147b878d064eea33b458a51e263ab6c55
Zachary <zacarlin@gmail.com>
2019-09-08 18:25:28 -0400

    added background color to comopoonetns

--- src/components/portfolioCard.vue
+++ src/components/portfolioCard.vue
@@ -3,21 +3,25 @@
         <a-assets>
             <img id="floor" src="https://res.cloudinary.com/dybluctxg/image/upload/v1495307503/field_kcw4so.jpg">
             <img id="github" src="https://res.cloudinary.com/dybluctxg/image/upload/v1495310880/github_ipn5do.png" alt="">
-            <img id="me" src="../assets/beefJerky.jpg">
+            <img id="me" src="../assets/miamiHomeGirl.jpg">
             <img id="accubrew" src="../assets/AccubrewIMG.png">
             <img id="about7sharks" src="../assets/about7sharks.png">
         </a-assets>
         <a-sky src='#floor'></a-sky>
-        <a-plane  color="#CCC" height="3.2" width="3">
-                <a-circle @click="hey()" src='#me' position="0 .5 0.01" radius=".7"></a-circle>
+        <a-plane  color="#80c1ff" height="3.4" width="3">
+                <a-circle @click="hey()" src='#me' position="0 .5 0.01" radius=".6"></a-circle>
                 <a-circle @mousedown="goGit()" src='#github' position="-1 -1.35 0.01" radius=".25"></a-circle>
                 <a-circle @mousedown="goAccubrew()" src='#accubrew' position="0 -1.35 0.01" radius=".25"></a-circle>
                 <a-circle @mousedown="goAbout7Sharks()" src='#about7sharks' position="1 -1.35 0.01" radius=".25"></a-circle>
 
 
-            <a-text font="aileronsemibold" color=black width=5 align='center' value="Zachary Carlin"
-        position="0 1.35 0"></a-text>
-        <a-box position='0 -.27 .01' width=3 height=.1 depth=.001></a-box>
+        <a-text font="aileronsemibold" color=black width=5 align='center' value="ZACHARY CARLIN"
+        position="0 1.4 0"></a-text>
+        <a-box color=#004280 position='0 -.27 .01' width=3 height=.071 depth=.001></a-box>
+        <a-box color=#004280 position='-1.5 0 .01' rotation='0 0 90' width=3.3 height=.1 depth=.001></a-box>
+        <a-box color=#004280 position='1.5 0 .01' rotation='0 0 90' width=3.3 height=.1 depth=.001></a-box>
+        <a-box color=#004280 position='0 1.7 .01' width=3.1 height=.1 depth=.001></a-box>
+        <a-box color=#004280 position='0 -1.7 .01' width=3.1 height=.1 depth=.001></a-box>
 
          <a-text font="aileronsemibold" color=black width=2.8 align='center' value="Hi, there i'm a Software Engineer based in Tampa, FL. For the last year i've been working at Gulf Fiberoptics on Accubrew.io a craft beer fermentation monitoring system and in my free time I enjoy tinkering with webxr technology and Machine Learning"
         position="0 -.7 0"></a-text>
